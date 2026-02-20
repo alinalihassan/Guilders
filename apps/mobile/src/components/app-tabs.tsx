1e53c8d66f7c2a88c3af620495df17fc7456a88a
Alin Ali Hassan <hassanalinali@gmail.com>
2026-02-20 12:01:42 +0100

    Platform specific icons

--- apps/mobile/src/components/app-tabs.tsx
+++ apps/mobile/src/components/app-tabs.tsx
@@ -1,6 +1,6 @@
 import { NativeTabs } from 'expo-router/unstable-native-tabs';
 import React from 'react';
-import { useColorScheme } from 'react-native';
+import { Platform, useColorScheme } from 'react-native';
 
 import { Colors } from '@/constants/theme';
 
@@ -15,12 +15,18 @@ export default function AppTabs() {
       labelStyle={{ selected: { color: colors.text } }}>
       <NativeTabs.Trigger name="index">
         <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon src={require('@/assets/images/tabIcons/home.png')} />
+        {Platform.select({
+          ios: <NativeTabs.Trigger.Icon sf={{ default: 'house', selected: 'house.fill' }} />,
+          android: <NativeTabs.Trigger.Icon md="home" />,
+        })}
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="explore">
         <NativeTabs.Trigger.Label>Settings</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon src={require('@/assets/images/tabIcons/explore.png')} />
+        {Platform.select({
+          ios: <NativeTabs.Trigger.Icon sf="gearshape" />,
+          android: <NativeTabs.Trigger.Icon md="settings" />,
+        })}
       </NativeTabs.Trigger>
     </NativeTabs>
   );
