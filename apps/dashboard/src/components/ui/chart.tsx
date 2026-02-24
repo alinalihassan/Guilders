f9906d0c64f5cdf87bf0cadf08ab5edc9bfcdb43
Alin Ali Hassan <hassanalinali@gmail.com>
2026-02-24 17:02:08 +0100

    Fixed warnings

--- apps/dashboard/src/components/ui/chart.tsx
+++ apps/dashboard/src/components/ui/chart.tsx
@@ -58,7 +58,12 @@ const ChartContainer = React.forwardRef<
         {...props}
       >
         <ChartStyle id={chartId} config={config} />
-        <RechartsPrimitive.ResponsiveContainer>
+        <RechartsPrimitive.ResponsiveContainer
+          width="100%"
+          height="100%"
+          minWidth={0}
+          minHeight={1}
+        >
           {children}
         </RechartsPrimitive.ResponsiveContainer>
       </div>
